b04db476ccd52b9aaa19056ba8369b4113429393
LJS0000 <leejisu2281@gmail.com>
2024-10-15 11:29:49 +0900

    💄 UI_레이아웃 요소 수정 및 반응형 적용

--- src/pages/CreatePage.tsx
+++ src/pages/CreatePage.tsx
@@ -1,4 +1,5 @@
 const CreatePage = () => {
+  //title, desc, word list(10~30 at least 10words)
   return <div>Create</div>;
 };
 export default CreatePage;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,14 +2,6 @@ import styles from '../styles/HomePage.module.css';
 import Board from '../components/shared/Board';
 
 const HomePage = () => {
-  return (
-    <div className={styles.home}>
-      <aside className={`${styles.sidebar} ${styles.leftSide}`}>??</aside>
-      <main className={styles.mainContainer}>
-        <Board />
-      </main>
-      <aside className={`${styles.sidebar} ${styles.rightSide}`}>??</aside>
-    </div>
-  );
+  return <Board />;
 };
 export default HomePage;
